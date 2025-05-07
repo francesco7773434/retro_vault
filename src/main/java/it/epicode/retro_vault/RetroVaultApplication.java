720521e95c19ee177fcfbbea33e2263f89556d56
francesco7773434 <francescoorbello5@gmail.com>
2025-05-07 07:56:54 +0200

    Initial commit

---
+++ src/main/java/it/epicode/retro_vault/RetroVaultApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.retro_vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/retro_vault/RetroVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.retro_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
