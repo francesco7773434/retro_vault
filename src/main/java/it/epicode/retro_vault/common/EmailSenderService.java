2dfc490637187ecd24ec90bb35e2d9c3136b0a31
francesco7773434 <francescoorbello5@gmail.com>
2025-05-15 17:45:12 +0200

    modifiche

--- src/main/java/it/epicode/retro_vault/common/EmailSenderService.java
+++ src/main/java/it/epicode/retro_vault/common/EmailSenderService.java
@@ -8,23 +8,35 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import java.util.Properties;
+
 @Service
 public class EmailSenderService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendEmail(String to, String subject, String body) throws MessagingException {
+    public EmailSenderService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+
+        // Se il mailSender è di tipo JavaMailSenderImpl, puoi forzare le proprietà qui
+        if (mailSender instanceof org.springframework.mail.javamail.JavaMailSenderImpl) {
+            org.springframework.mail.javamail.JavaMailSenderImpl senderImpl =
+                    (org.springframework.mail.javamail.JavaMailSenderImpl) mailSender;
+            Properties props = senderImpl.getJavaMailProperties();
+            props.put("mail.smtp.ssl.trust", "*");
+            props.put("mail.smtp.ssl.checkserveridentity", "false");
+        }
+    }
+
+    public void sendEmail(String to, String subject, String text) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message);
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
-        // Splitta la stringa "to" usando virgola o punto e virgola come delimitatori
-        String[] recipients = to.split("\\s*[,;]\\s*");
-        helper.setTo(recipients);
-
+        helper.setTo(to);
         helper.setSubject(subject);
-        helper.setText(body, true);
+        helper.setText(text, true);
+
         mailSender.send(message);
-        System.out.println("Email inviata con successo a " + to);
     }
 
     public void sendEmailWithAttachment(String to, String subject, String body, byte[] attachmentBytes, String attachmentName) throws MessagingException {
